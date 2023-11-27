484aaf6bcc03917172bbe157d80ca7cee16d4994
goldengun1 <simihailo@gmail.com>
2023-11-27 18:25:49 +0100

    initial commit

---
+++ bash/.custom_bash_configurations.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# alias currentbranch='git branch | grep \* | sed "s/* //"'
+blk='\[\033[01;30m\]'   # Black
+red='\[\033[01;31m\]'   # Red
+grn='\[\033[01;32m\]'   # Green
+ylw='\[\033[01;33m\]'   # Yellow
+blu='\[\033[01;34m\]'   # Blue
+pur='\[\033[01;35m\]'   # Purple
+cyn='\[\033[01;36m\]'   # Cyan
+wht='\[\033[01;37m\]'   # White
+clr='\[\033[00m\]'      # Reset
+
+function git_branch() {
+    if [ -d .git ] ; then
+        printf "%s" "($(git branch 2> /dev/null | awk '/\*/{print $2}'))";
+    fi
+}
+
+function bash_prompt(){
+    # PS1='${debian_chroot:+($debian_chroot)}'${blu}'$(git_branch)'${pur}' \W'${grn}' \$ '${clr}
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(git_branch)'${clr}'\$ '
+}
+
+bash_prompt
